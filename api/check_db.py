0fb5a24ab3fd852e391eadb411aa03d3a69edbb3
Aryan13705 <aj6923@srmist.edu.in>
2026-03-18 22:14:20 +0530

    Update

--- api/check_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("schemes.db")
-cursor = conn.cursor()
-
-cursor.execute("SELECT COUNT(*) FROM central_schemes")
-print("Total rows:", cursor.fetchone()[0])
-
-conn.close()
--- api/main.py
+++
@@ -1,9 +0,0 @@
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
-from app import app
-
-initialize_app()
-
-@https_fn.on_request()
-def benefitai_api(req: https_fn.Request) -> https_fn.Response:
-    return app
